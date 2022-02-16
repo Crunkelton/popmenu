9994bb3838682052d20b28d9c7e5797bdf4a141f
Joshua Lee <jlee6.1988@gmail.com>
2022-02-16 14:08:25 -0500

    Adding additional testing to the menu service to test fetches and additions.

--- src/app/services/menu.service.spec.ts
+++ src/app/services/menu.service.spec.ts
@@ -2,18 +2,42 @@ import { TestBed } from '@angular/core/testing';
 
 import { MenuService } from './menu.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MenuItem } from '../models/menu-item';
+import { mockMenuItems } from '../testing/mockMenuItems';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemMenuItemService } from './in-mem-menu-item.service';
 
 describe('MenuService', () => {
   let service: MenuService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
+      imports: [
+        HttpClientTestingModule,
+        HttpClientInMemoryWebApiModule.forRoot(InMemMenuItemService),
+      ]
     });
     service = TestBed.inject(MenuService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should match the mock menu item results',
+    (done: DoneFn) => {
+      service.getMenuItems().subscribe((value: MenuItem[]) => {
+        expect(value).toEqual(mockMenuItems);
+        done();
+      });
+    });
+
+  const newMenuItem = {id: 100, title: 'new item', description: 'new descirption', image: 'http://myurl.com', price: 1.99 };
+
+  it('should add a new menu item', (done: DoneFn) => {
+    service.addMenuItem(newMenuItem).subscribe((value: MenuItem) => {
+      expect(value).toEqual(newMenuItem);
+      done();
+    });
   });
 });
